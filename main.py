cff761b3dff8170121812472cf55aa5964f29a7f
АД <degterevastasa@gmail.com>
2022-09-30 09:37:58 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from PIL import Image
+import numpy as np
+
+
+def load_image(infilename):
+    img = Image.open(infilename)
+    img.load()
+    data = np.asarray(img, dtype="int32")
+    return data
+
+
+def save_image(npdata, outfilename):
+    img = Image.fromarray(np.asarray(np.clip(npdata, 0, 255), dtype="uint8"), "L")
+    img.save(outfilename)
+
+
+if __name__ == '__main__':
+    a = load_image('imgs/sample.bmp')
+    print(a.shape)
+
---
+++ nn/NeuralNetworkClass.py
@@ -0,0 +1,3 @@
+class NeuralNetwork:
+    def __init__(self, l_rate=0.01):
+        pass
