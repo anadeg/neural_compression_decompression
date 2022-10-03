e8d28e0075930cd2f72ade713902b7c9ec0d9aa6
АД <degterevastasa@gmail.com>
2022-10-03 13:26:11 +0300

    main matrix version

--- main.py
+++ main.py
@@ -15,6 +15,62 @@ def save_image(npdata, outfilename):
 
 
 if __name__ == '__main__':
-    a = load_image('imgs/sample.bmp')
-    print(a.shape)
+    # a = load_image('imgs/sample.bmp')
+    # print(a.shape)
+    # print(a)
+    # a = a.reshape(a.shape[0]*a.shape[1], a.shape[2])
+    # print(a.shape)
+    # print(a)
+    """
+    X = [1, 2, 3]
+    W = [[0.8, 0.3],
+        [-0.2, 0.9]]
+    """
+    # X = np.array([[1, 1], [2, 2], [3, 3]])
+    # W = np.array([[[0.4, 0.5], [-0.1, 0.9]],
+    #             [[-0.9, -0.9], [0.3, 0.8]],
+    #             [[0.3, -0.3], [0.6, -0.2]]])
+    # Y_temp = X.dot(W)
+
+    # X = np.array([1, 2, 2])
+    # W = np.array([[0.4, 0.9],
+    #               [-0.9, 0.3],
+    #               [-0.1, 0.5]])
+    # Y_temp = X.dot(W)
+
+    # X = np.array([[1, 1], [2, 2], [3, 3]]).T
+    # print(X.shape)
+    # W = np.array([[[0.4, 0.4], [0.9, 0.9]],
+    #               [[-0.9, -0.9], [0.3, 0.3]],
+    #               [[-0.1, -0.1], [0.5, 0.5]]]).T
+    # # W = np.array([[0.4, 0.4], [-0.7, -0.7], [-0.2, -0.2]])
+    # print(W.shape)
+    # print(W, '\n\n')
+
+    # X = np.array([1, 2, 3, 4])
+    # W = np.array([[0.2, -0.1],
+    #               [0.6, 0.1],
+    #               [-0.8, 0.2],
+    #               [0.4, -0.4]])
+
+    X = np.array([[1, 1], [2, 2], [3, 3], [4, 4]]).T
+    print(X, '\n\n')
+    # W = np.array([[[0.2, 0.2], [-0.1, -0.1]],
+    #               [[0.6, 0.6], [0.1, 0.1]],
+    #               [[-0.8, -0.8], [0.2, 0.2]],
+    #               [[0.4, 0.4], [-0.4, -0.4]]]).T
+    W = np.array([[0.2, -0.1],
+                  [0.6, 0.1],
+                  [-0.8, 0.2],
+                  [0.4, -0.4]])
+
+    Y_temp = np.dot(X, W)
+    print(Y_temp, '\n\n')
+    Y_temp = np.matmul(X, W)
+    print(Y_temp)
+    # Y_temp = np.dot(X, W)
+
+    # print(np.matmul([1, 1], [[0.4, 0.4], [0.9, 0.9]]))
+
+
 
