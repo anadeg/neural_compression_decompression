9fadaff5c06686dd3a28b7a6987c7d28ee95faf2
АД <degterevastasa@gmail.com>
2022-10-18 16:50:18 +0300

    version 7(?) colored, but if main color

--- nn/NeuralNetworkClass.py
+++ nn/NeuralNetworkClass.py
@@ -25,14 +25,20 @@ class NeuralNetwork:
         # change x
         # self.X = X.reshape(self.size, self.dim) / 255
         # self.X = X.reshape(self.size, self.dim)
-        self.X = X
-        self.Y = self.X
-        self.W1 = self.he_initialization(self.size, hidden_layers, self.size)
-        self.W2 = self.he_initialization(self.hidden_layers, self.size, hidden_layers)
-        self.b1 = np.zeros((hidden_layers, 1))
-        self.b2 = np.zeros((self.size, 1))
+        self.X = self.compress(X)
 
-    def compress(self):
+        self.new_h, self.new_w, d = self.X.shape
+        self.new_size = self.new_h * self.new_w
+
+        self.X = self.X.reshape(self.new_size, self.dim) / 255
+        self.Y = self.X
+
+        self.W1 = self.he_initialization(self.new_size, hidden_layers, self.new_size) / self.coefficient
+        self.W2 = self.he_initialization(self.hidden_layers, self.new_size, hidden_layers) / self.coefficient
+        self.b1 = np.zeros((hidden_layers, 1))
+        self.b2 = np.zeros((self.new_size, 1))
+
+    def compress(self, x):
         self.c_coeff = 4
         new_h, new_w = self.h // self.c_coeff, self.w // self.c_coeff
         new_x = np.zeros((new_h, new_w, self.dim))
@@ -41,7 +47,7 @@ class NeuralNetwork:
                 for d in range(self.dim):
                     start_i = self.c_coeff * i
                     start_j = self.c_coeff * j
-                    temp = self.X[start_i: start_i + self.c_coeff, start_j: start_j + self.c_coeff, d]
+                    temp = x[start_i: start_i + self.c_coeff, start_j: start_j + self.c_coeff, d]
                     value = temp.mean()
                     new_x[i, j, d] = value
         return new_x
@@ -106,15 +112,8 @@ class NeuralNetwork:
     def forward_propagation(self, X):
         Z1 = np.dot(self.W1, X) + self.b1
         A1 = self.leaky_relu(Z1)
-        # A1 = self.sigmoid(Z1)
-        # A1 = self.tanh(Z1)
-        # A1 = self.parametric_relu(Z1)
         Z2 = np.dot(self.W2, A1) + self.b2
-        # A2 = self.stable_softmax(Z2)
-        A2 = self.sigmoid(Z2)
-        # A2 = self.tanh(Z2)
-        # A2 = self.leaky_relu(Z2)
-        # A2 = np.copy(Z2)
+        A2 = np.copy(Z2)
 
         return Z1, Z2, A1, A2
 
@@ -122,18 +121,12 @@ class NeuralNetwork:
         Y_observed = self.Y
 
         dA2 = A2 - Y_observed
-        dZ2 = dA2 * self.sigmoid_derivative(Z2)
-        # dZ2 = self.leaky_relu_derivative(Z2)
-        # dZ2 = dA2 * self.tanh_derivative(Z2)
-        # dZ2 = A2 - Y_observed
+        dZ2 = dA2.copy()
         dW2 = np.dot(dZ2, A1.T)
         dB2 = 1 / dZ2.shape[1] * np.sum(dZ2, axis=1).reshape(-1, 1)
 
         dA1 = np.dot(self.W2.T, dZ2)
         dZ1 = dA1 * self.leaky_relu_derivative(Z1)
-        # dZ1 = dA1 * self.sigmoid_derivative(Z1)
-        # dZ1 = dA1 * self.tanh_derivative(Z1)
-        # dZ1 = dA1 * self.parametric_relu_derivative(Z1)
         dW1 = np.dot(dZ1, self.X.T)
         dB1 = 1 / dZ1.shape[1] * np.sum(dZ1, axis=1).reshape(-1, 1)
 
@@ -149,7 +142,7 @@ class NeuralNetwork:
         self.b1 = self.b1 - tempB1
         self.b2 = self.b2 - tempB2
 
-    def gradient_descent(self, learning_rate=0.001, iterations=30):
+    def gradient_descent(self, learning_rate=1*10**(-4), iterations=30):
         for i in range(1, iterations+1):
             if i % 5 == 0:
                 print(f'{i}-th iteration')
@@ -157,47 +150,38 @@ class NeuralNetwork:
             dW1, dW2, dB1, dB2 = self.backward_propagation(Z1, Z2, A1, A2)
             self.update_parameters(dW1, dW2, dB1, dB2, learning_rate=learning_rate)
 
-    def fit(self, learning_rate=2*10**(-4), iterations=50):
+    def fit(self, learning_rate=1*10**(-5), iterations=200):
         self.gradient_descent(learning_rate=learning_rate, iterations=iterations)
 
     def predict(self, X):
-        X = X.reshape(self.size, self.dim) / 255
         _, _, A1, A2 = self.forward_propagation(X)
-        # A2 = np.where(A2 >= 0, A2, 0)
 
-        # A1 = A1.reshape(30, 20, 3)
-        # A1 = np.round(255 * A1)
-        # save_image(A1, "RGB", 's')
-
-        A2 = A2.reshape(self.h, self.w, self.dim)
+        A2 = A2.reshape(self.new_h, self.new_w, self.dim)
         A2 = np.round(255 * A2)
-        # save_image(A2, "RGB", 's')
+        A2 = np.where(A2 > 255, 255, A2)
+        A2 = np.where(A2 < 0, 0, A2)
         return A2
 
 
 if __name__ == '__main__':
-    # data = load_image('../imgs/flower.jpg')
-    # # data = np.random.randint(255, size=(300, 200, 3))
-    # # print(data, '\n\n=====================\n')
-    #
-    # simple_nn = NeuralNetwork(data, hidden_layers=30*20)
-    # simple_nn.fit()
-    # output = simple_nn.predict(data)
-    # print(data[:5, :5])
-    # print(output[:5, :5])
-    #
-    # # # output = np.where(output >= 0, output, 0)
-    # # # output = output.reshape(3, 3, 3).round()
-    # # save_image(data, "RGBA", 'ffkfk')
-    # save_image(output, "RGB", '../imgs/kitten_output.bmp')
+    data = load_image('../imgs/kitten.jpg')
 
-    data_test = np.array([[[1, 1], [2, 1], [3, 1], [4, 1], [2, 2], [3, 2], [4, 2], [5, 2]],
-                          [[2, 1], [3, 1], [4, 1], [1, 1], [1, 2], [1, 2], [0, 2], [1, 2]],
-                          [[3, 1], [3, 1], [1, 1], [1, 1], [0, 2], [0, 2], [1, 2], [3, 2]],
-                          [[8, 1], [5, 1], [6, 1], [4, 1], [3, 2], [2, 2], [2, 2], [1, 2]]])
-    test_nn = NeuralNetwork(data_test, hidden_layers=10)
-    # print(data_test[[2, 2, 0], [0, 4, 0]])
-    n_d_test = test_nn.compress()
+    simple_nn = NeuralNetwork(data, hidden_layers=80*40)
+    simple_nn.fit()
+    output = simple_nn.predict(simple_nn.X)
+    print(data[:5, :5])
+    print(output[:5, :5])
+
+    save_image(output, "RGB", '')
+
+    # data_test = np.array([[[1, 1], [2, 1], [3, 1], [4, 1], [2, 2], [3, 2], [4, 2], [5, 2]],
+    #                       [[2, 1], [3, 1], [4, 1], [1, 1], [1, 2], [1, 2], [0, 2], [1, 2]],
+    #                       [[3, 1], [3, 1], [1, 1], [1, 1], [0, 2], [0, 2], [1, 2], [3, 2]],
+    #                       [[8, 1], [5, 1], [6, 1], [4, 1], [3, 2], [2, 2], [2, 2], [1, 2]]])
+    # test_nn = NeuralNetwork(data, hidden_layers=10)
+    # # print(data_test[[2, 2, 0], [0, 4, 0]])
+    # n_d_test = test_nn.compress()
+    # # print(n_d_test[20: 25, 20: 25, :])
 
 
 
